d0cfc37451005f0bb71f57d1d539d8ccaa795092
inukawaii <mayumi.td@gmail.com>
2019-06-12 23:42:31 +0900

    bluma追加 TODO追加エリアを追加 追加されたTODO用のコンポーネントを追加

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>TODOリスト</h1>
+    <input type="checkbox" id="invisible_check" v-model="done_is_invisible">
+    <label for="invisible_check">完了したTODOは非表示にする</label>
+    <ul>
+      <Todo
+        v-for="(todo, index) in todos"
+        v-bind:key="index"
+        v-bind:title="todo.title"
+        v-bind:rep="todo.rep"
+        v-bind:members="members"
+        v-bind:done_is_invisible="done_is_invisible"
+      />
+      <li>
+        <input type="textarea" v-model="new_todo" placeholder="TODOを入力してください">
+        <select v-model="rep" size="1">
+          <option value='' disabled selected>担当者を選択</option>
+          <option v-for="member in members" v-bind:value="member" v-bind:key="member">
+            {{member}}
+          </option>
+        </select>
+        <input type="date" v-model="new_date">
+        <button v-on:click="add">追加</button>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Todo from "./components/Todo.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    HelloWorld
+    Todo
+  },
+  data: function() {
+    return {
+      members: ["さざえ", "かつお", "わかめ"],
+      new_todo: "",
+      new_date: null,
+      todos: [],
+      rep: '',
+      done_is_invisible: false
+    };
+  },
+  methods: {
+    add: function() {
+      if(this.valid) {
+        this.todos.push(
+          {
+            title: this.new_todo,
+            date: this.date,
+            rep: this.rep
+          }
+        )
+        this.new_todo = ''
+        this.new_date = null
+        this.rep = null
+      }
+    }
+  },
+  computed: {
+    valid: function() {
+      return this.new_todo != ''
+    }
   }
-}
+};
 </script>
 
-<style>
+<style lang="scss">
+@import "../node_modules/bulma/bulma.sass";
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ src/components/Todo.vue
@@ -0,0 +1,32 @@
+<template>
+  <li v-show="!done_is_invisible || !is_done">  
+    <input type="checkbox" v-model="is_done">  
+      <span>
+          <input type="text" v-model="title" v-bind:disabled="is_done">
+      </span>
+    <select v-bind:disabled="is_done" v-model="rep">
+        <option v-for="member in members" v-bind:value="member" v-bind:key="member">{{member}}</option>
+    </select>
+    <input type="date" v-model="date" v-bind:disabled="is_done">
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    members: Array,
+    title: String,
+    date: Date,
+    rep: String,
+    done_is_invisible: Boolean
+  },
+  data: function() {
+      return {
+        is_done: false,
+      }
+  }
+}
+</script>
+
+<style scoped>
+</style>
